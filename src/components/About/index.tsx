a2daafabe498a31d366f93be02b02330850b2067
Rafael C. Vedana <rafael.dvedana@gmail.com>
2025-12-14 17:52:36 -0300

    little changes

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,14 +1,20 @@
+import faceIcon from "../../assets/face-icon.png";
 type AboutProps = {
   children: React.ReactNode;
 };
 
 export function About({ children }: AboutProps) {
-  return <div className="">
-    <div>
-      <img src="./src/assets/profile.png" alt="" />
+  const containerClasses = "bg-neutral-950 rounded-2xl p-5 "
+
+  return (
+    <div className="flex gap-2">
+      <div className={containerClasses}>
+        <img src={faceIcon} alt="" className="w-50"/>
+      </div>
+
+      <div className={containerClasses}>
+        <div>{children}</div>
+      </div>
     </div>
-    <div>
-    {children}
-    </div>
-    </div>;
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgrPlugin from "vite-plugin-svgr";
+
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgrPlugin()],
   base: "/Portifolio",
 });
